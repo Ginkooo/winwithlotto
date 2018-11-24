849c43d6f33700581c1caeb06903195b0a9d850b
Medford5852 <andriy5852@gmail.com>
2018-11-24 18:13:57 +0100

    add filter scratch position

--- app/src/main/java/pl/wrzosdev/findscratchwin/ScratchController.java
+++ app/src/main/java/pl/wrzosdev/findscratchwin/ScratchController.java
@@ -11,25 +11,47 @@ import java.util.ArrayList;
 
 public class ScratchController {
 
-    public static ArrayList<Scratch> getAllScratches() throws IOException {
-
+    public static ArrayList<Scratch> getData(String url) throws IOException {
         OkHttpClient client = new OkHttpClient();
 
-            Request request = new Request.Builder()
-                    .url("http://10.250.193.25:8000/api/scratches/")
-                    .build();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
 
-            Response response = client.newCall(request).execute();
+        Response response = client.newCall(request).execute();
 
-            Scratch[] scratches;
-            Gson gson = new Gson();
-            scratches = gson.fromJson(response.body().string(), Scratch[].class);
+        Scratch[] scratches;
+        Gson gson = new Gson();
+        scratches = gson.fromJson(response.body().string(), Scratch[].class);
 
-            ArrayList<Scratch> scratchArrayList = new ArrayList<>();
-            for (Scratch scratch: scratches) {
-                scratchArrayList.add(scratch);
-            }
+        ArrayList<Scratch> scratchArrayList = new ArrayList<>();
+        for (Scratch scratch: scratches) {
+            scratchArrayList.add(scratch);
+        }
 
-            return scratchArrayList;
+        return scratchArrayList;
+    }
+
+    public static ArrayList<Scratch> getAllScratches() throws IOException {
+        return getData("http://10.250.193.25:8000/api/scratches/");
+    }
+
+    public static ArrayList<Scratch> getAddScratchesBetweenLatitude(double latitudeLte, double latitudeGte) throws IOException {
+        return getData("http://10.250.193.25:8000/api/scratches/?latitude__lte="
+                + latitudeLte + "&latitude__gte=" + latitudeGte + "&longtitude__lte=&longtitude__gte=");
+    }
+
+    public static ArrayList<Scratch> getAddScratchesBetweenLongtitude(double longtitudeLte, double longtitudeGte) throws IOException {
+        return getData("http://10.250.193.25:8000/api/scratches/?latitude__lte=&latitude__gte=&longtitude__lte="
+                + longtitudeLte + "&longtitude__gte=" + longtitudeGte);
+    }
+
+    public static ArrayList<Scratch> getAddScratchesBetweenLatitudeAndLongtitude(double latitudeLte,
+                                                                                 double latitudeGte,
+                                                                                 double longtitudeLte,
+                                                                                 double longtitudeGte) throws IOException {
+        return getData("http://10.250.193.25:8000/api/scratches/?latitude__lte="
+                + latitudeLte + "&latitude__gte=" + latitudeGte + "&longtitude__lte="
+                + longtitudeLte + "&longtitude__gte=" + longtitudeGte);
     }
 }
